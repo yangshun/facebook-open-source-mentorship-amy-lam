77114b32be902cc8acea44f6cca25b89b537393f
Yangshun Tay <tay.yang.shun@gmail.com>
2018-05-04 22:34:45 -0700

    Implement livereload

--- examples/mini-docusaurus/server.js
+++ examples/mini-docusaurus/server.js
@@ -1,7 +1,11 @@
 const app = require('express')();
 const fs = require('fs');
+const gaze = require('gaze');
+const tinylr = require('tiny-lr');
 const remarkable = require('remarkable');
 const md = new remarkable();
+
+const PORT = 35729;
 
 // ----- Allow CORS
 app.use(function(req, res, next) {
@@ -23,9 +27,27 @@ app.get('*', function(req, res) {
       </head>
       <body>
         ${convertedFile}
+        <script src="http://localhost:${PORT}/livereload.js"></script>
       </body>
     </html>
   `);
 });
 
 app.listen(5000);
+
+const server = tinylr();
+server.listen(PORT, function() {
+  console.log('Livereload listening on %s ...', PORT);
+})
+
+// gaze watches some specified files and triggers a callback when they change.
+gaze('docs/**/*', function () {
+  // Trigger on all kinds of file changes - modified/added/deleted.
+  this.on('all', function () {
+    // Notify LiveReload clients that there's a change.
+    // Typically, LiveReload will only refresh the changed paths.
+    // Hence we use / here so that the clients are informed that
+    // the root has changed, so they will do a full-page refresh.
+    server.notifyClients(['/']);
+  });
+});
